c7cd43b83ebc8600dfcd98a3ef8eb8ffbd69ea9e
Amati Angelo <amati.angelo@20m2official.it>
2024-02-27 16:32:57 +0100

    add domain configured to accept https requests

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: true,
     title: '20m2 Dashboard Prod',
     //TODO: quando lo compreremo qui ci sarà da configurare il server (e quindi il dominio in https per comunicare in cloud)
-    apiURL: 'https://20m2datacloud.com:8088/',
+    apiURL: 'https://20m2datacloud.com:8088',
     hostname: 'https://www.20m2consulting.it/'
 };
