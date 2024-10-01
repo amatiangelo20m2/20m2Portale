c69d401793557c8d3a87af1f7ffd0c2b777a5da6
amatiangelo20m2 <amati.angelo@20m2official.it>
2024-10-01 20:33:09 +0200

    refactor

---
+++ src/app/modules/admin/restaurant/tag/tag.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-tag',
+  templateUrl: './tag.component.html',
+  standalone: true
+})
+export class TagComponent implements OnInit, OnDestroy{
+  ngOnDestroy(): void {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
